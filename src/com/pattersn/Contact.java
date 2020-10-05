471819e4dc8b008a3ecca10cb40f2fabd02fbc46
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-05 15:18:59 +0530

    [Arka]Added:Java Streams

---
+++ src/com/pattersn/Contact.java
@@ -0,0 +1,54 @@
+package com.pattersn;
+
+
+import java.util.*;
+
+
+public class Contact {
+
+    public String firstname;
+    public String lastname;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public String number;
+    public String email;
+    public Contact(String firstname, String lastname, String address, String city, String state, String zip, String number,
+                   String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.number = number;
+        this.email = email;
+    }
+    public Contact()
+    {
+
+    }
+    public void Display()
+    {
+        System.out.println(firstname+" "+lastname+" "+address+" "+city+" "+state+" "+zip+" "+number+" "+email);
+    }
+    public String getCity()
+    {
+        return city;
+    }
+    public String getState()
+    {
+        return state;
+    }
+    public String getName()
+    {
+        return firstname;
+    }
+
+
+
+
+
+
+}
