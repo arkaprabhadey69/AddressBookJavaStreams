1fd0492767a62178165754e459d9562d42eb79f4
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-14 14:04:21 +0530

    [Arka]Added:Ability to count entries from file

--- src/com/pattersn/AddressBookMainTest.java
+++ src/com/pattersn/AddressBookMainTest.java
@@ -25,5 +25,21 @@ public class AddressBookMainTest {
 
 
     }
+    @Test
+    public void given_Contacts_Should_Write_To_A_File_And_Give_Count(){
+        Contact[] contacts={new Contact("Arka","Dey","lmao","kol","wb","700026","8961377960","abc@gmail.com" ),
+                new Contact("Mayuri","Dey","lmao","kol","wb","700089","8961388960","abcd@gmail.com" ),
+                new Contact("Nayanika","Guha","lmao","kol","wb","700096","8961377900","abcdef@gmail.com" )};
+        ArrayList<Contact> arr=new ArrayList<>();
+        arr.add(contacts[0]);
+        arr.add(contacts[1]);
+        arr.add(contacts[2]);
+        AddressBookMain adressBook= new AddressBookMain();
+        adressBook.writeData(arr);
+        long entries=adressBook.countEntries();
+        Assert.assertEquals(3,entries);
+
+
+    }
 
 }
